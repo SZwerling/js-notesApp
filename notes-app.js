2395db56dbbebaad57f0153cef173debcfabd61a
unknown <sazwerling@gmail.com>
2022-05-22 07:55:24 -0500

    form example

--- notes-app.js
+++ notes-app.js
@@ -18,7 +18,9 @@ const renderNotes = (notesObj, filtersObj) => { //takes arr of objects and a 'fi
    const filteredNotes = notesObj.filter((note) => {  //compares arr obj 'title' prop with filters obj 'searchText' prop
       return note.title.toLowerCase().includes(filtersObj.searchText.toLowerCase())
    })
+
    document.querySelector('#notes').innerHTML = '' //clears all text from div w id 'text'
+
    filteredNotes.forEach((item) => {  // for object in arr that matched
       const noteP = document.createElement('p')  //we create a 'p' element
       noteP.textContent = item.title             //give it the text from the title prop
@@ -34,11 +36,14 @@ document.querySelector('#create-note').addEventListener("click", (e) => {
    console.log(e.target)
 })
 
-document.querySelector('#delete-notes').addEventListener('click', (e) => {
-   document.querySelectorAll('#notes').forEach((note) => note.remove())
-})
 
 document.querySelector('#search-text').addEventListener('input', (e) => {
    filters.searchText = e.target.value //updating property on filters object
    renderNotes(notes, filters) //calling renderNotes function with 
+})
+
+document.querySelector('#name-form').addEventListener('submit', (e) => {  //query selector on form id
+   e.preventDefault()  //preventing the default behavior of page refreshing
+   console.log(e.target.elements.firstName.value)  //"elements" allows access to fields of form // firstName is name of input // value is what client input
+   e.target.elements.firstName.value = ''
 })
